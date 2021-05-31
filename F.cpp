701df0d68b6fc621a3c743c928d1e2505ffc80cd
Melly-chan <74918377+Melly-chan@users.noreply.github.com>
2021-05-31 13:29:29 +0300

    Update F.cpp

--- F.cpp
+++ F.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
-#include <cmath>
+#include <fstream>
 using namespace std;
-int main(){
-    int a, b, sum = 0, max = 0;
-		cin >> a;
-		cin >> b;
-		for (int k = a; k <= b; k++){
-		    if (k <= 10 && k >= -10)
-		    if (k > 10 || k < -10){
-		        int h = k, p = 0;
-		        while(h > 0){
-		            h = h/10;
-		            p++;
-		        }
-		        int m = k/pow(10, p-2);
-		        int n = k%100;
-		        int v = m - n;
-		        if ((m - n)!=0 && k%v == 0){
-		            sum++;
-		            if (k > max) {max = k;}
-		            
-		        }
-			}
-			
-		}
-	cout << sum;
-	cout << max;
-	return 0;
+
+int A(int n) {
+    while (n > 100) {
+        n = n/10;
+    }return n;
+}
+int main() {
+    int a, b, m;
+    cin >> a >> b;
+    int max = 0;
+    int sum = 0;
+    while (a < b) {
+        if (a < 100) {
+            m = 0;
+        } else {
+            m = A(a) - a% 100;
+        } if (m!=0){
+            if (a%m == 0) {
+                sum = sum + 1;
+                max = a;
+            }
+        } a++;
+    }cout << sum << " " << max;
+    return 0;
 }
